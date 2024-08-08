309e25e302ca3e878fa250d3c1f1260c743433d3
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:27:48 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Get started!
+const greeting = "Welcome to Truthful";
+console.log(greeting);
+let grade = prompt("what is your grade? " + "'Im in "+ " 9, 10,11,12' ")
+grade =parseInt(grade)
+console.log(typeof grade)
+
+let username = prompt("What is your username ? ")
+console.log("Hello " + username)
+
+let firstQuestion = prompt("Is it true that you attend CompSciHigh " + username + "?");
+
+let secondQuestion = promp("is it true that your are in Software Engineering ?");
+
+
+
